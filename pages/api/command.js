fcdc54782465d22582995f9e076f8c1b1de0aeb6
Jo du Plessis <jo@joduplessis.com>
2020-04-09 10:11:14 +0200

    Manual GQL call on API

--- pages/api/command.js
+++ pages/api/command.js
@@ -1,9 +1,6 @@
 import Cors from 'cors'
 import moment from 'moment'
-
-/**
- * Template for a POST call
- */
+import axios from 'axios'
 
 // Initializing the cors middleware
 const cors = Cors({
@@ -35,11 +32,53 @@ async function handler(req, res) {
   const commandQueryParts = commandQuery.split(',')
   const title = commandQueryParts[0]
   const description = commandQueryParts[1]
-  const options = commandQueryParts.splice(2, (commandQueryParts.length - 1))
-  const expiry = moment().add(1, 'months').toDate()
+  const expiry = moment().add(1, 'months').format('YYYY-MM-DD 00:00:00')
+  const options = commandQueryParts
+    .splice(2, (commandQueryParts.length - 1))
+    .map((option, index) => {
+      return {
+        id: index,
+        text: option,
+      }
+    })
+
+  // Make a manual GRaphQL request
+  // Bit of a hack - but saves having to jump through the GQL-via-server hoops
+  await axios({
+    url: 'https://yack-apps.herokuapp.com/v1/graphql',
+    method: 'post',
+    data: {
+      "operationName": "add_poll",
+      "variables": {
+        "objects": [
+          {
+            "title": title,
+            "description": description,
+            "options": options,
+            "expiry": expiry,
+            "channel_token": token,
+            "user_id": userId
+          }
+        ]
+      },
+      "query": `
+        mutation add_poll($objects: [polls_insert_input!]!) {
+          insert_polls(objects: $objects) {
+            returning {
+              id
+              title
+              __typename
+            }
+            __typename
+          }
+        }
+      `
+    }
+  })
+  console.log(poll)
 
   // Rest of the API logic
-  res.json({ userId, token, title, description, options })
+  res.json({ success: true })
 } catch (e) {
   console.log(e)
   res.json({ error: true })
