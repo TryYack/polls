3db90925d2079bc780b35dcb3b43cd2605747249
Jo du Plessis <jo@joduplessis.com>
2020-04-24 11:09:33 +0200

    Removed Serverless

--- next.config.js
+++ next.config.js
@@ -1,11 +1,1 @@
-const GenerateAwsLambda = require('next-aws-lambda-webpack-plugin')
-
-module.exports = {
-  target: 'serverless',
-  /*
-  webpack: (config, nextConfig) => {
-    config.plugins.push(new GenerateAwsLambda(nextConfig))
-    return config
-  }
-  */
-}
+module.exports = {}
