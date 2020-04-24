2dc382919365d927d4178d58f12790a22c87d2d5
Jo du Plessis <jo@joduplessis.com>
2020-04-24 10:18:46 +0200

    Added serverless configs

--- next.config.js
+++ next.config.js
@@ -2,8 +2,10 @@ const GenerateAwsLambda = require('next-aws-lambda-webpack-plugin')
 
 module.exports = {
   target: 'serverless',
+  /*
   webpack: (config, nextConfig) => {
     config.plugins.push(new GenerateAwsLambda(nextConfig))
     return config
   }
+  */
 }
